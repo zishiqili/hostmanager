9e5f36a1d07d39da2bec669d5ac5e231e0b2156f
zishiqili <36766837+zishiqili@users.noreply.github.com>
2025-05-08 22:35:48 +0800

    Update middleware.py

--- hostmanager/middleware.py
+++ hostmanager/middleware.py
@@ -1,16 +1,17 @@
 import time
+from django.utils.deprecation import MiddlewareMixin
 
-class RequestTimingMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        start_time = time.time()
-
-        response = self.get_response(request)
-
-        duration = time.time() - start_time
-        print(duration)
-        response["X-Request-Duration-ms"] = f"{int(duration * 1000)}"
-
+class RequestTimingMiddleware(MiddlewareMixin):
+    """中间件用于统计请求处理耗时"""
+    
+    def process_request(self, request):
+        """在请求开始时记录时间"""
+        request.start_time = time.time()
+    
+    def process_response(self, request, response):
+        # 计算耗时（毫秒）
+        total_time = (time.time() - request.start_time) * 1000
+        print(total_time)
+        response['X-Request-Time'] = f'{total_time:.2f}ms'
+        
         return response
